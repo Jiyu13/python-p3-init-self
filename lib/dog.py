6b5b5aa8ecbad784e26f15e2aa41dbfc45fac912
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 19:09:39 -0500

    iniital commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    # initialise the class
+    def __init__(self, name, favorite_toy="Any"):
+        self.name = name
+        self.favorite_toy = favorite_toy
+
+    
+    def bark(self):
+        print("Woof!")
+
+
+    def showing_self(self):
+        return self
+
+    def get_adpoted(self, owner_name):
+        self.owner = owner_name
+    
+
+fido = Dog("Fido")
+fido.name    # Fido
+print(fido.showing_self())  # <__main__.Dog object at 0x7f49a9a4f7c0>
+
+# set fido owner attritube
+fido.get_adpoted("Sophie")
+print(fido.owner)   # Sophie
+
+fido.favorite_toy # Any
+
+
+snoopy = Dog("Snoopy", "Tennis Ball")
+snoopy.name
+snoopy.favorite_toy # Tennis Ball
